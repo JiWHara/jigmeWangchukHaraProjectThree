1886bf7370d48269b22d4d095ace0ad4af01bb86
Jigme Hara <jigme.hara@gmail.com>
2023-03-28 17:44:05 -0400

    added some logic to access meme api and display images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+// import MemeSelector from './components/MemeSelector.js';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // declared memeImages state variable using useState and used currentMemeImages function to update state image as
+  // data fetched from imgflip api
+  const [memeImage, setMemeImage] = useState([]);
+  let currentMemeImage = 0;
+  
+  useEffect(() => {
+    axios({
+      url: "https://api.imgflip.com/get_memes",
+      method: "GET",
+      dataResponse: "json",
+    }).then((res) => {
+    const memes = res.data.data.memes;
+    const memeUrl = memes[currentMemeImage].url;
+    const memeName = memes[currentMemeImage].name;
+    
+    setMemeImage(memeUrl);
+
+    console.log("First meme URL:", memeUrl);
+    console.log("First meme name:", memeName);
+
+    })
+  }, [])
+
+return (
+  <div className="App">
+    <header>
+      <h1>TEST</h1>
+      <div>
+      {memeImage && <img src={memeUrl} alt={memeName}/>}
+      </div>
+    </header>
+  </div>
+  )
 }
 
 export default App;
+// imgflip user and password for post param 
+// user: terryfox231
+// pass: bananabutt123
